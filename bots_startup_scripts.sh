986362657c95f6d9a0642e6db4d51428a5b98033
Cloud User <ec2-user@ip-172-31-24-218.us-east-2.compute.internal>
2022-06-11 10:53:40 +0000

    added logsbots_startup_scripts.sh

--- bots_startup_scripts.sh
+++ bots_startup_scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # command to run this file: 
 
-nohup python main.py >> nohup_discord.out &                # running discord bot
-nohup python viber_bot/__init__.py  >> nohup_viber.out &   # running viber_bot
-nohup python telegram_bot/__init__.py  >> nohup_telegram.out &   # running telegram bot outputs_to_file: nohup_telegram.out
+nohup python main.py >> logs/nohup_discord.out &                # running discord bot
+nohup python viber_bot/__init__.py  >> logs/nohup_viber.out &   # running viber_bot
+nohup python telegram_bot/__init__.py  >> logsnohup_telegram.out &   # running telegram bot outputs_to_file: nohup_telegram.out
 
