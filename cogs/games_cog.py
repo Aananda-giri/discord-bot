f3fc282aa21697bdbca80237c7a05ef47bf39b35
aananda-giri <aanandaprashadgiri@gmail.com>
2023-05-16 10:47:12 +0545

    Game: Count is working now

--- cogs/games_cog.py
+++ cogs/games_cog.py
@@ -57,7 +57,8 @@ class Games(commands.Cog, name="games"):
                 
                 title = 'count Stopped'
                 description = f'description: \n channel_id: {context.channel.id} \n current_count: {count_data["current_count"]} \n last_counter: {count_data["last_counter"]} \n current_score: {count_data["current_score"]} \n highest_score: {count_data["highest_score"]}'
-                embed = get_embeded_message(context, title, description)
+                # embed = get_embeded_message(context, title, description)
+                embed = get_embeded_message(context, 'title', 'description', author=True)
                 
                 await context.send(embed = embed)
             else:
