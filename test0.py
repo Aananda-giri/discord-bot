4de2c5b2a693b40795028b3819c1338ce5ca3ef9
aananda-giri <aanandaprashadgiri@gmail.com>
2023-05-27 18:43:20 +0545

    test

--- test0.py
+++ test0.py
@@ -3,12 +3,13 @@ import interactions
 import config
 bot = interactions.Client(token=config.DISCORD_TEST_BOT_TOKEN, application_id=config.DISCORD_TEST_BOT_ID)
 
-# @bot.command(
-@interactions.slash_command(
+@bot.command(
+# @interactions.slash_command(  # v5
     name="my_first_command",
     description="This is the first command I made!",
 )
-async def my_first_command(ctx: interactions.SlashContext):
+# async def my_first_command(ctx: interactions.SlashContext):   # v5
+async def my_first_command(ctx: interactions.CommandContext):
     await ctx.send("Hi there!")
 
 bot.start()
