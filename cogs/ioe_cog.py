c75c6ae6e6173ee5f7e22cd090c1d951c95439d3
aananda-giri <aanandaprashadgiri@gmail.com>
2023-05-27 23:05:13 +0545

    seperate cog for ioe

--- cogs/ioe_cog.py
+++ cogs/ioe_cog.py
@@ -1,11 +1,9 @@
-import discord, asyncio
-import math, asyncpraw, asyncprawcore
+
 from discord.ext import commands
 from cogs.functions import get_embeded_message
 # from replit import db
 from database import db
-import config
-
+/io
 class IoeCommands(commands.Cog, name="reddit_commands"):
     def __init__(self, bot):
         self.bot = bot    
@@ -22,7 +20,7 @@ class IoeCommands(commands.Cog, name="reddit_commands"):
         embed = get_embeded_message(context, 'unleashing ioe_notifications: {message}')
         await context.send(embed=embed)
 
-    @ioe_notice.command(name='stop_ioe_notice',
+    @ioe_notice.command(name='stop',
                  brief='to contain/stop unleashed  subreddit message',
                  help='e.g. `.contain jokes`')
     async def stop(self, context):
@@ -36,7 +34,7 @@ class IoeCommands(commands.Cog, name="reddit_commands"):
                 embed = get_embeded_message(context, f'stopped ioe_notifications: {message}')
             else:
                 embed = get_embeded_message(context, 'ioe_notifications not started to {}'.format(context.channel))
-            await context.send(embed=embed,empheral=True)
+            await context.channel.send(embed=embed,empheral=True)
             
 async def setup(bot):
     await bot.add_cog(IoeCommands(bot))
