e47075b1f7fcddeee7fe6fd1fae5e49f4972bc68
aananda-giri <aanandaprashadgiri@gmail.com>
2023-05-27 22:25:16 +0545

    migration to hybrid commands

--- cogs/reddit_cog.py
+++ cogs/reddit_cog.py
@@ -214,5 +214,34 @@ class RedditCommands(commands.Cog, name="reddit_commands"):
           #print(context.channel.id)
             #await context.send(context.channel.id)
 
+
+    @commands.hybrid_group(name='ioe_notice',
+                 aliases=['ioe_noti', 'ioe_notification', 'ioe_notifications', 'ioenoti', 'ioenotification', 'ioenotifications'],
+                 brief='unleahes the subreddit to the channel',
+                 help='e.g.To unleash r/jokes `.unleash jokes`',
+                 fallback="start")
+    async def ioe_notice(self, context, subreddit, how_many=8):
+        print('\n\n ------------- IOE-Notice initiated ------------- \n\n')
+        ioe_db = db('ioe_notifications')
+        message = ioe_db.add_one(str(context.channel.id))
+        embed = get_embeded_message(context, 'unleashing ioe_notifications: {message}')
+        await context.send(embed=embed)
+
+    @ioe_notice.command(name='stop_ioe_notice',
+                 brief='to contain/stop unleashed  subreddit message',
+                 help='e.g. `.contain jokes`')
+    async def stop(self, context, subreddit):
+            # stop ioe notification from channel
+            ioe_db = db('ioe_notifications')
+            if int(context.channel.id) in ioe_db.get_all():
+                
+                channel_id = str(context.channel.id)
+                message = ioe_db.remove_one(channel_id)
+                
+                embed = get_embeded_message(context, f'stopped ioe_notifications: {message}')
+            else:
+                embed = get_embeded_message(context, 'ioe_notifications not started to {}'.format(context.channel))
+            await context.send(embed=embed,empheral=True)
+            
 async def setup(bot):
     await bot.add_cog(RedditCommands(bot))
