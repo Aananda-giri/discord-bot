777306546e95fccf29095d8a8106f7386ccaac9e
Aananda-Giri <60064717+Aananda-giri@users.noreply.github.com>
2024-03-01 10:54:49 +0545

    raw-stats

--- cogs/stats_cog.py
+++ cogs/stats_cog.py
@@ -88,12 +88,13 @@ class Stats(commands.Cog, name="stats"):
           1132858904582311946, 1154660261106552832
       ]
       today = datetime.now()
-      # start_of_day = datetime(today.year, today.month, today.day)
+      start_of_day = datetime(today.year, today.month, today.day)
       start_of_week = today - timedelta(days=today.weekday()+1)
 
       message_dict = {}
       reaction_dict = {}
       # Iterate over all text channels in the server
+      print(f'ctx.guild')
       for channel in ctx.guild.text_channels:
       # for channel in ctx.guild.get_all_channels():
         if int(channel.id) in channels_to_exclude:
@@ -102,20 +103,28 @@ class Stats(commands.Cog, name="stats"):
         try:
           # List all the messages sent in the channel today
           messages = channel.history(after=start_of_day)
+          print(f'messages:{messages}')
           async for message in messages:
             time.sleep(.2)
             if not message.author.bot:
+              print(f'channel:{channel.name} \n\n message:{message}\n\nfucking reactions: {message.reactions}')
+            #   print(f'author:{message.reactions[0].author} \n\n ')
+              for reaction in  message.reactions:
+                async for user in reaction.users():
+                    if str(user) not in reaction_dict:
+                        reaction_dict[str(user)] = 1
+                    else:
+                        reaction_dict[str(user)] += 1
+
               # Dont display messages by bot
               if message.channel.name not in message_dict:
                 message_dict[message.channel.name] = []
-              if message.channel.name not in reaction_dict:
-                reaction_dict[message.channel.name] = []
               # Append reactions to reaction_dict
               
               message_dict[message.channel.name].append(
                   {
-                      'author': message.author.name,
-                      'content': message.content
+                      'author': str(message.author.name),
+                      'content': str(message.content)
                   }
               )  # 'created_at': message.created_at, 'channel_id': message.channel.id})
               print(
@@ -124,67 +133,22 @@ class Stats(commands.Cog, name="stats"):
         except Exception as e:
           print(f"Couldn't fetch history from {channel.name}, {e}")
 
-      message_count = count_messages(messages = message_dict, how_many = 15)
-      await ctx.author.send(
-          f'## Here is lis of messages today to the server:{ctx.guild.name}`\n {str(message_dict)[:1900]}`',
-          silent=True)
+      message_count = count_messages(messages = message_dict, how_many = -1)
+      reactions_count = sorted(reaction_dict.items(), key=lambda x: x[1], reverse=True)
+      # Save as json file
+      
+      with open("messsage_reaction_count.json",'w') as f:
+        json.dump({"message_count": message_count, 'reactions_count':reactions_count},f)
+      # Send the file to user
+      await ctx.author.send(file=discord.File("messsage_reaction_count.json"))
+      #   await ctx.author.send(
+      #       f'## Here is lis of messages today to the server:{ctx.guild.name}`\n {str(message_dict)[:1900]}`',
+      #       silent=True)
       print(message_count)
       msg_embed = create_stylish_leaderboard_embed(message_count, question_expired=False, is_most_active_leaderboard=True)
 
       await ctx.send(embed=msg_embed, silent=True)
 
-
-
-
-
-
-
-
-
-
-
-
-      # message_dict = {}
-      # reaction_dict = {}
-      # # Iterate over all text channels in the server
-      # for channel in ctx.guild.text_channels:
-      # # for channel in ctx.guild.get_all_channels():
-      #   if int(channel.id) in channels_to_exclude:
-      #     continue
-      #   time.sleep(.2)
-      #   try:
-      #     # List all the messages sent in the channel today
-      #     messages = channel.history(after=start_of_day)
-      #     async for message in messages:
-      #       time.sleep(.2)
-      #       if not message.author.bot:
-      #         # Dont display messages by bot
-      #         if message.channel.name not in message_dict:
-      #           message_dict[message.channel.name] = []
-      #         if message.channel.name not in reaction_dict:
-      #           reaction_dict[message.channel.name] = []
-      #         # Append reactions to reaction_dict
-
-      #         message_dict[message.channel.name].append(
-      #             {
-      #                 'author': message.author.name,
-      #                 'content': message.content
-      #             }
-      #         )  # 'created_at': message.created_at, 'channel_id': message.channel.id})
-      #         print(
-      #             f'{message.author.name} : {message.content} : {message.created_at}'
-      #         )
-      #   except Exception as e:
-      #     print(f"Couldn't fetch history from {channel.name}, {e}")
-
-      # message_count = count_messages(messages = message_dict, how_many = 15)
-      # await ctx.author.send(
-      #     f'## Here is lis of messages today to the server:{ctx.guild.name}`\n {str(message_dict)[:1900]}`',
-      #     silent=True)
-      # print(message_count)
-      # msg_embed = create_stylish_leaderboard_embed(message_count, question_expired=False, is_most_active_leaderboard=True)
-
-      # await ctx.send(embed=msg_embed, silent=True)
     
 async def setup(bot):
   await bot.add_cog(Stats(bot))
